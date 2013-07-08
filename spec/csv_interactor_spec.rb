807448bd41fa7c3a46860e26440d2735e74578c7
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2013-07-08 13:28:22 -0600

    CSV loads all posts, loads title -> failing

--- spec/csv_interactor_spec.rb
+++ spec/csv_interactor_spec.rb
@@ -7,4 +7,13 @@ describe "CSV interactor" do
     all_posts = CSV.read("lib/posts.csv")
     all_posts.first.should_equal ["Post 1", "Description of post 1"]
   end
+
+  describe "all posts" do
+    it "loads title" do
+      post = Post.new(title: "Post 1", description: "Description of post 1")
+      CSVInteractor.save_post post
+      all_posts = CSVInteractor.all_posts
+      all_posts.first.title.should_equal "Post 1"
+    end
+  end
 end
