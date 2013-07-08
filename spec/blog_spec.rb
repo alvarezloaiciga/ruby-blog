0c2cdf96791f7ccb792eb5f8dd3ee00c90f82458
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2013-07-08 08:24:21 -0600

    Blog adds post description -> Passing

--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -17,5 +17,11 @@ describe "blog" do
       blog.add_post(title: "First post in blog", description: "This is a magical post")
       blog.posts.first.title.should_equal "First post in blog"
     end
+
+    it "adds description" do
+      blog = Blog.new(name: "Blog 1")
+      blog.add_post(title: "First post in blog", description: "This is a magical post")
+      blog.posts.first.description.should_equal "This is a magical post"
+    end
   end
 end
