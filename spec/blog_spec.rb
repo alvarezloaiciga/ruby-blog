c933b245c51b04003ed3a625003f907d85346a8d
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2013-07-07 23:57:04 -0600

    Add posts to a blog -> failing

--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -10,4 +10,12 @@ describe "blog" do
     blog = Blog.new(name: "Blog 1")
     blog.posts.should_equal []
   end
+
+  describe ".add_post" do
+    it "adds title" do
+      blog = Blog.new(name: "Blog 1")
+      blog.add_post(title: "First post in blog", description: "This is a magical post")
+      blog.posts.first.title.should_equal "First post in blog"
+    end
+  end
 end
