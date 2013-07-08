6f234f3e52404076d029b78a62c66c9e5e8ae7d2
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2013-07-07 23:51:04 -0600

    Post has a description -> failing

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -6,4 +6,10 @@ describe "Post" do
     post = Post.new(title: title)
     post.title.should_equal title
   end
+
+  it "has a description" do
+    description = "This is my first post!"
+    post = Post.new(description: description)
+    post.description.should_equal description
+  end
 end
