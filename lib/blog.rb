14022a8479e83d23bcbb48a8f52493316e1d7a06
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2013-07-08 13:06:36 -0600

    Removes name from blog

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,7 +1,6 @@
 class Blog
   attr_reader :name, :posts
   def initialize args
-    @name = args[:name]
     @posts = []
   end
 
--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -1,11 +1,6 @@
 require_relative 'spec_helper'
 
 describe "blog" do
-  it "has a name" do
-    blog = Blog.new(name: "Blog 1")
-    blog.name.should_equal "Blog 1"
-  end
-
   it "has posts" do
     blog = Blog.new(name: "Blog 1")
     blog.posts.should_equal []
