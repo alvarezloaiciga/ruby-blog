1d98441697faf894a391f5f57291f70dcd29dce1
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2013-07-08 13:32:24 -0600

    CSV loads all posts, loads title -> passing

--- lib/csv_interactor.rb
+++ lib/csv_interactor.rb
@@ -5,4 +5,9 @@ class CSVInteractor
       csv << [post.title,post.description]
     end
   end
+
+  def self.all_posts
+    posts = CSV.read("lib/posts.csv")
+    posts.map{|post_attr| Post.new(title: post_attr[0])}
+  end
 end
