07da0ed7b8c72794d72d704d9a4133ecab41d504
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2013-07-08 20:48:12 -0600

    Assigns id to post on save -> failing

--- spec/csv_interactor_spec.rb
+++ spec/csv_interactor_spec.rb
@@ -1,11 +1,19 @@
 require_relative 'spec_helper'
 
 describe "CSV interactor" do
-  it "saves a post" do
-    post = Post.new(title: "Post 1", description: "Description of post 1")
-    CSVInteractor.save_post post
-    all_posts = CSV.read("lib/posts.csv")
-    all_posts.first.should_equal ["Post 1", "Description of post 1"]
+  describe "saves a post" do
+    it "assigns an id to the post" do
+      post = Post.new(title: "Post 1", description: "Description of post 1")
+      post = CSVInteractor.save_post post
+      post.id.should_equal 1
+    end
+
+    it "saves a post" do
+      post = Post.new(title: "Post 1", description: "Description of post 1")
+      CSVInteractor.save_post post
+      all_posts = CSV.read("lib/posts.csv")
+      all_posts.first.should_equal ["Post 1", "Description of post 1"]
+    end
   end
 
   describe "all posts" do
