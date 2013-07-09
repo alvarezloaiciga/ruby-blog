47018bab08316248b8610b840dc8676037d545c3
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2013-07-08 20:44:22 -0600

    Post has a nil id on new -> passing

--- lib/post.rb
+++ lib/post.rb
@@ -1,6 +1,6 @@
 class Post
-  attr_reader :title, :description
-  def initialize args
+  attr_reader :title, :description, :id
+  def initialize(args={})
     @title = args[:title]
     @description = args[:description]
   end
