6f8c83be7e13882336b89be0c816f0377d0219f3
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2013-07-08 21:36:07 -0600

    Refactor next id on CSV interactor

--- lib/csv_interactor.rb
+++ lib/csv_interactor.rb
@@ -1,18 +1,10 @@
 require 'csv'
 class CSVInteractor
   def self.save_post post
-    if File.exist?("lib/posts.csv")
-      id = CSV.read("lib/posts.csv").count + 1
-      post.send(:id=, id)
-    else
-      post.send(:id=, 1)
-    end
-
-
     CSV.open("lib/posts.csv", "ab") do |csv|
+      post.send(:id=, next_id)
       csv << [post.id, post.title,post.description]
     end
-
     post
   end
 
@@ -20,4 +12,14 @@ class CSVInteractor
     posts = CSV.read("lib/posts.csv")
     posts.map{|post_attr| Post.new(id:post_attr[0], title: post_attr[1], description: post_attr[2])}
   end
+
+  private
+  def self.next_id
+    begin
+      return CSV.read("lib/posts.csv").count + 1
+    rescue
+      return 1
+    end
+  end
+
 end
