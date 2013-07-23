8206629b87f8483ed538a7ad29638c6070c6ac41
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2013-07-23 13:24:57 -0600

    Load blog on posts when loading all of them -> passing

--- lib/csv_interactor.rb
+++ lib/csv_interactor.rb
@@ -10,7 +10,10 @@ class CSVInteractor
 
   def self.all_posts
     posts = CSV.read("lib/posts.csv")
-    posts.map{|post_attr| Post.new(id:post_attr[0].to_i, title: post_attr[1], description: post_attr[2])}
+    posts.map{|post_attr| Post.new(id:post_attr[0].to_i,
+                                   title: post_attr[1],
+                                   description: post_attr[2],
+                                   blog_id: post_attr[3].to_i)}
   end
 
   private
