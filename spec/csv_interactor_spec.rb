465eca66d646d502ab7be1da80c9065aad29bf89
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2013-07-23 13:10:47 -0600

    Refactor csv interactor spec with constants

--- spec/csv_interactor_spec.rb
+++ spec/csv_interactor_spec.rb
@@ -1,11 +1,12 @@
 require_relative 'spec_helper'
+require_relative 'constants'
 
 describe "CSV interactor" do
   File.delete('lib/posts.csv') if File.exist?('lib/posts.csv')
 
   describe "saves a post" do
     it "assigns an id to the post" do
-      post = Post.new(title: "Post 1", description: "Description of post 1")
+      post = Post.new(title: POST_TITLE, description: POST_DESCRIPTION)
       post = CSVInteractor.save_post post
       post.id.should_equal 1
 
@@ -13,10 +14,10 @@ describe "CSV interactor" do
     end
 
     it "saves a post" do
-      post = Post.new(title: "Post 1", description: "Description of post 1")
+      post = Post.new(title: POST_TITLE, description: POST_DESCRIPTION)
       CSVInteractor.save_post post
       all_posts = CSV.read("lib/posts.csv")
-      all_posts.first.should_equal ["1","Post 1", "Description of post 1"]
+      all_posts.first.should_equal ["1",POST_TITLE, POST_DESCRIPTION]
 
       File.delete('lib/posts.csv')
     end
@@ -24,7 +25,7 @@ describe "CSV interactor" do
 
   describe "all posts" do
     it "loads title" do
-      post = Post.new(title: "Post 1", description: "Description of post 1")
+      post = Post.new(title: POST_TITLE, description: POST_DESCRIPTION)
       CSVInteractor.save_post post
       all_posts = CSVInteractor.all_posts
       all_posts.first.title.should_equal post.title
@@ -33,7 +34,7 @@ describe "CSV interactor" do
     end
 
     it "loads description" do
-      post = Post.new(title: "Post 1", description: "Description of post 1")
+      post = Post.new(title: POST_TITLE, description: POST_DESCRIPTION)
       CSVInteractor.save_post post
       all_posts = CSVInteractor.all_posts
       all_posts.first.description.should_equal post.description
@@ -42,7 +43,7 @@ describe "CSV interactor" do
     end
 
     it "loads id" do
-      post = Post.new(title: "Post 1", description: "Description of post 1")
+      post = Post.new(title: POST_TITLE, description: POST_DESCRIPTION)
       CSVInteractor.save_post post
       all_posts = CSVInteractor.all_posts
       all_posts.first.id.should_equal post.id
