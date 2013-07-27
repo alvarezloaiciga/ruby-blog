1fc1ea3f5141ec605c28d70100546d9789a6643c
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2013-07-27 17:31:33 -0600

    New post page form -> failing

--- spec/web/blog/post_servlet_spec.rb
+++ spec/web/blog/post_servlet_spec.rb
@@ -6,12 +6,28 @@ require 'net/http'
 describe "Post Servlet" do
   initialize_test_server
   describe "do_GET" do
-    it "loads all posts titles for the blog" do
-      blog = Blog.new
-      blog.add_post(title: POST_TITLE, description: POST_DESCRIPTION)
-      response = Net::HTTP.get('localhost', "/posts?blog_id=#{blog.id}&id=#{blog.posts.first.id}", 3000)
-      response.strip.should_equal "<h1>#{POST_TITLE}</h1>\n\n<p>#{POST_DESCRIPTION}</p>"
-      File.delete('lib/posts.csv')
+    describe "show" do
+      it "loads all posts titles for the blog" do
+        blog = Blog.new
+        blog.add_post(title: POST_TITLE, description: POST_DESCRIPTION)
+        response = Net::HTTP.get('localhost', "/posts?blog_id=#{blog.id}&id=#{blog.posts.first.id}", 3000)
+        response.strip.should_equal "<h1>#{POST_TITLE}</h1>\n\n<p>#{POST_DESCRIPTION}</p>"
+        File.delete('lib/posts.csv')
+      end
+    end
+
+    describe "new" do
+      it "loads the the form for a new post" do
+        blog = Blog.new
+        response = Net::HTTP.get('localhost', "/posts/new?blog_id=#{blog.id}", 3000)
+        response.strip.should_equal(
+        "<form name=\"new-post\" action=\"/posts\" method=\"post\">\n" +
+        "Title: <input type=\"text\" name=\"title\"><br>\n" +
+        "Description: <input type=\"text\" name=\"description\">\n" +
+        "<input type=\"hidden\" name=\"blog_id\" value=2>\n"+
+        "<input type=\"submit\" value=\"Save\">\n"+
+        "</form>")
+      end
     end
   end
 
