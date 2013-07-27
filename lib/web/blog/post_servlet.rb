7fa1b3f63cb4e67c1b055de179ce8dbba8d80aa1
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2013-07-27 17:31:43 -0600

    New post page form -> passing

--- lib/web/blog/post_servlet.rb
+++ lib/web/blog/post_servlet.rb
@@ -4,6 +4,31 @@ include WEBrick
 
 class PostServlet < HTTPServlet::AbstractServlet
   def do_GET request, response
+    if is_new_path? request.path
+      render_new_template request, response
+    else
+      render_show_template request, response
+    end
+  end
+
+  def do_POST request, response
+    blog = Blog.find(request.query["blog_id"].to_i)
+    blog.add_post(title: request.query["title"],
+                 description: request.query["description"])
+    response.set_redirect(WEBrick::HTTPStatus[303], "/blog?id=#{blog.id}")
+  end
+
+  private
+  def render_new_template request, response
+    File.open('lib/web/blog/new_post.rhtml', 'r') do |f|
+      @template = ERB.new(f.read)
+    end
+    @blog_id = request.query["blog_id"]
+    response.body = @template.result(binding)
+    response['Content-Type'] = "text/html"
+  end
+
+  def render_show_template request, response
     File.open('lib/web/blog/post_show.rhtml', 'r') do |f|
       @template = ERB.new(f.read)
     end
@@ -13,10 +38,8 @@ class PostServlet < HTTPServlet::AbstractServlet
     response['Content-Type'] = "text/html"
   end
 
-  def do_POST request, response
-    blog = Blog.find(request.query["blog_id"].to_i)
-    blog.add_post(title: request.query["title"],
-                 description: request.query["description"])
-    response.set_redirect(WEBrick::HTTPStatus[303], "/blog?id=#{blog.id}")
+  def is_new_path? path
+    path_sections = path[1..-1].split "/"
+    path_sections[1] && path_sections[1] == "new"
   end
 end
